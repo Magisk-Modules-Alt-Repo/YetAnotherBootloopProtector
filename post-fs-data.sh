f2f211d540301f2fc74fd2ccebba6b769e489e9f
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-01-12 17:07:41 +0530

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,9 +1,26 @@
 DIR="${0%/*}"
+description() {
+    for module_dir in /data/adb/modules/*/; do
+        MODULE_PROP="$module_dir/module.prop"
+        if [ -f "$MODULE_PROP" ]; then
+            sed -i '/^description=/c\description=Module Was Disabled Because a Bootloop was detected.' "$MODULE_PROP"
+        fi
+    done
+}
+permissions() {
+    for dir in /data/adb/post-fs-data.d /data/adb/service.d; do
+        if [ -d "$dir" ]; then
+            find "$dir" -type f -exec chmod 644 {} \;
+        fi
+    done
+}
 if [ -f "$DIR/s1" ] && [ -f "$DIR/s2" ] && [ -f "$DIR/s3" ]; then
     rm -f "$DIR/s1" "$DIR/s2" "$DIR/s3"
     for module_dir in /data/adb/modules/*/; do
         touch "$module_dir/disable"
     done
+    permissions
+    description
     reboot
 elif [ -f "$DIR/s2" ]; then
     touch "$DIR/s3"
