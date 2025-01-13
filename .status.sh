1be58bc47280b34d7c5847e111fddd63e23acdfc
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-01-13 12:35:14 +0530

    Update .status.sh

--- .status.sh
+++ .status.sh
@@ -8,19 +8,19 @@ REQUIRED_FILES="\
     $MODULE_DIR/uninstall.sh"
 for file in $REQUIRED_FILES; do
     if [ ! -f "$file" ]; then
-        sed -i '/^description=/d' "$MODULE_PROP"
+        sed -i'' '/^description=/d' "$MODULE_PROP"
         echo "description= ⚠️ Warning : Module is corrupted. Please reinstall." >> "$MODULE_PROP"
         touch "$MODULE_DIR/disable"
         exit 0
     fi
 done
 if [ -f "$MODULE_DIR/disable" ]; then
-    sed -i '/^description=/d' "$MODULE_PROP"
+    sed -i'' '/^description=/d' "$MODULE_PROP"
     echo "description= SystemUI Monitor ❌ | Bootloop Monitor ❌ | ⚠️ Module is Disabled " >> "$MODULE_PROP"
 elif [ -f "$SYSTEMUI_MONITOR_DISABLE" ]; then
-    sed -i '/^description=/d' "$MODULE_PROP"
+    sed -i'' '/^description=/d' "$MODULE_PROP"
     echo "description= SystemUI Monitor ❌ | Bootloop Monitor ✅ | Module is Working ✨" >> "$MODULE_PROP"
 else
-    sed -i '/^description=/d' "$MODULE_PROP"
+    sed -i'' '/^description=/d' "$MODULE_PROP"
     echo "description= SystemUI Monitor ✅ | Bootloop Monitor ✅ | Module is Working ✨" >> "$MODULE_PROP"
 fi
